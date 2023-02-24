18d2ab0c50d7c3fe710f3e3f6146f0b74b1db1b9
Tuanno <tuannodanyllo26@gmail.com>
2023-02-24 14:33:02 -0300

    alterações no código do script e index

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,29 +1,49 @@
 // Menu Mobile
+
 function showMenu() {
-    let menuMobile = document.querySelector('.menu-mobile');
-    let menuIconOpen = document.querySelector('.fa-bars');
-    let menuIconClose = document.querySelector('.fa-xmark');
-  
-    if (menuMobile.classList.contains('open')) {
-      menuMobile.classList.remove('open');
-  
-      menuIconOpen.classList.remove('closeMenuIcon');
-      menuIconClose.classList.add('closeMenuIcon');
-    } else {
-      menuMobile.classList.add('open');
-  
-      menuIconOpen.classList.add('closeMenuIcon');
-      menuIconClose.classList.remove('closeMenuIcon');
-    }
+  let menuMobile = document.querySelector('.menu-mobile');
+  let menuIconOpen = document.querySelector('.fa-bars');
+  let menuIconClose = document.querySelector('.fa-xmark');
+
+  if (menuMobile.classList.contains('open')) {
+    menuMobile.classList.remove('open');
+
+    menuIconOpen.classList.remove('closeMenuIcon');
+    menuIconClose.classList.add('closeMenuIcon');
+  } else {
+    menuMobile.classList.add('open');
+
+    menuIconOpen.classList.add('closeMenuIcon');
+    menuIconClose.classList.remove('closeMenuIcon');
   }
-
-
+}
 
 // About us Tab
-function getAboutUs(){
-    fetch('https://www.mdsystemweb.com.br/projects/api/v1/test/about.php')
+
+window.onload = function() {
+  let descendentes = document.querySelectorAll(".header a");
+    
+  for (let i = 0; i < descendentes.length; i++) {
+    descendentes[i].addEventListener("click", function (event) {
+      // alert('O elemento clicado foi o ' + this.innerHTML);
+      
+      fetch('https://www.mdsystemweb.com.br/projects/api/v1/test/about.php')
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => {
+          if (this.innerHTML === 'Missão') {
+            return document.querySelector('#response').innerText = data[0].description;
+          }
+
+          if (this.innerHTML === 'Visão') {
+            return document.querySelector('#response').innerText = data[1].description;
+          }
+
+          if (this.innerHTML === 'Valores') {
+            return document.querySelector('#response').innerHTML = data[2].description;
+          }
+        });
+    })
+  }
 }
 
 // Services
@@ -32,5 +52,3 @@ function getServicesUs(){
       .then((response) => response.json())
       .then((data) => console.log(data));
 }
-
-
