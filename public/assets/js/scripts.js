808335b99d02e48ad4f448f037782e6ed9d2db46
Tuanno <tuannodanyllo26@gmail.com>
2023-02-24 11:46:01 -0300

    desafio landingpage

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,32 @@
+// Menu Mobile
+function showMenu() {
+    let menuMobile = document.querySelector('.menu-mobile');
+    let menuIconOpen = document.querySelector('.fa-bars');
+    let menuIconClose = document.querySelector('.fa-xmark');
+  
+    if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+  
+      menuIconOpen.classList.remove('closeMenuIcon');
+      menuIconClose.classList.add('closeMenuIcon');
+    } else {
+      menuMobile.classList.add('open');
+  
+      menuIconOpen.classList.add('closeMenuIcon');
+      menuIconClose.classList.remove('closeMenuIcon');
+    }
+  }
+
+
+
+// About us Tab
+function getAboutUs(){
+    fetch('https://www.mdsystemweb.com.br/projects/api/v1/test/about.php')
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+}
+
+// Services
+
+
+
